14fcc45db8a7495db9de11b3dc0492d2430c7fff
Iker Burguera <ikerburguera@gmail.com>
2016-09-27 18:50:20 +0200

    Update and rename launch.sh to launcher.sh

--- ReproducirVideosBoton/launcher.sh
+++ ReproducirVideosBoton/launcher.sh
@@ -5,6 +5,11 @@
 # 2- Navega al directorio home. 
 # 3- Despues desde este directorio navega al directorio a donde esta el script que vamos a ejecutat
 # 4- Una vez en el directorio ejecutamos el script y volvemos al directorio home
+#
+# Este Script se ejecuta al inicio con un CronJob
+#  sudo crontab -l 
+#  @reboot sh /home/pi/PythonScript/launcher.sh >> /home/pi/PythonScript/logs/cronlog 2>&1
+#
 
 sudo systemctl disable getty@tty1.service
 cd /
